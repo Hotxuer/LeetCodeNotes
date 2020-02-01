04df061b3e672416bfd0d3321a06602c315a59ab
nieliangxu <yk8288196@126.com>
2020-02-01 18:38:03 +0800

    question1

---
+++ Problem1.cpp
@@ -0,0 +1,42 @@
+//
+// Created by mac on 2020/2/1.
+//
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+// 暴力法
+class Solution1 {
+public:
+ vector<int> twoSum(vector<int>& nums, int target) {
+     int size = nums.size();
+     for (int i=0; i<size; i++) {
+         for (int j=i+1; j<size; j++) {
+             if (nums[i]+nums[j] == target) {
+                 vector<int> results;
+                 results.push_back(i);
+                 results.push_back(j);
+                 return results;
+             }
+         }
+     }
+     return vector<int>(2, -1);
+ }
+};
+
+// 哈希映射
+class Solution2 {
+public:
+vector<int> twoSum(vector<int>& nums, int target) {
+    map<int, int> numMap;
+    for (int i=0; i<nums.size(); i++) {
+        if (numMap.count(target - nums[i])) {
+            return {numMap[target - nums[i]], i};
+        }
+        numMap[nums[i]] = i;
+    }
+    return {-1, -1};
+}
+};
