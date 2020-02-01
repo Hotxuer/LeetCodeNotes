7b59373eb9416fb5903ac70c4f1fb94bb9d64476
nieliangxu <yk8288196@126.com>
2020-02-01 18:41:20 +0800

    添加依赖

---
+++ problem.h
@@ -0,0 +1,19 @@
+//
+// Created by mac on 2020/2/1.
+//
+
+#ifndef LEETCODENOTE_PROBLEM_H
+#define LEETCODENOTE_PROBLEM_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+#endif //LEETCODENOTE_PROBLEM_H
