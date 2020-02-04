d07ef4aaebd1eb948abc3a841bd0c98cfdbe7b2d
nieliangxu <yk8288196@126.com>
2020-02-04 22:45:37 +0800

    question7

---
+++ Problem7.cpp
@@ -0,0 +1,21 @@
+//
+// Created by mac on 2020/2/4.
+//
+
+#include "problem.h"
+
+// 纯数学方法，不转字符串，但要防止溢出
+class Solution {
+public:
+    int reverse(int x) {
+        long result = 0;
+        int max = 0x7fffffff;
+        int min = 0x80000000;
+        while (x != 0) {
+            result = result*10 + x%10;
+            x = x/10;
+        }
+        cout<<max<<" "<<min;
+        return (result > max || result < min) ? 0 : result;
+    }
+};
